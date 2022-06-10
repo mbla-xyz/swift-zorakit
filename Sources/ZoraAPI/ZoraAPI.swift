ddd9d3b8036e865c9c98df2d85181a6204bb47e8
Lee Adkins <lee@ravsonic.com>
2022-06-10 13:05:40 -0400

    leaving notes

--- Sources/ZoraAPI/ZoraAPI.swift
+++ Sources/ZoraAPI/ZoraAPI.swift
@@ -1,8 +1,34 @@
+import SwiftGraphQL
 
 public struct ZoraAPI {
+  public var endpoint = "https://api.zora.co/graphql"
+  
   public private(set) var text = "Hello, World!"
 
   public init() {
-    
   }
+  
+  func token() {
+      // addressses
+      // owners
+      // token + id
+      // filter
+      // pagination
+      // networks
+      // sort
+      // include full
+      // include sales
+  }
+  
+  func token() {
+    // token address
+    // token id
+    // network Chain
+    // netowrk Network
+    // include full
+  }
+  
+  
+  
+  
 }
